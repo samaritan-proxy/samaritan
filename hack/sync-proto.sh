c8adba72182a6ce27c16ad877389eca219dff9b9
kirk91 <kirk91.han@gmail.com>
2019-11-15 11:01:08 +0800

    copy go generated files

--- hack/sync-proto.sh
+++ hack/sync-proto.sh
@@ -22,17 +22,26 @@ readonly GIT_REPO="git@github.com:samaritan-proxy/samaritan-api"
 readonly DIR=".ci/samaritan-api"
 readonly PROTO_SOURCE_DIR="pb"
 readonly PROTO_TARGET_DIR="$DIR/proto"
+readonly GO_SOURCE_DIR="pb"
+readonly GO_TARGET_DIR="$DIR/go"
 
-# clone repo and copy the latest proto files
+# clone repo, copy the latest proto and go generated files.
 rm -rf $DIR
 git clone $GIT_REPO $DIR
-rsync -avz --exclude "*.go" $PROTO_SOURCE_DIR/ $PROTO_TARGET_DIR/
-pushd $DIR
+rsync -avz --include "*.proto" --include="*/" --exclude "*" $PROTO_SOURCE_DIR/ $PROTO_TARGET_DIR/
+rsync -avz --include "*.go" --include="*/" --exclude "*" $GO_SOURCE_DIR/ $GO_TARGET_DIR/
+cp -f go.mod $GO_TARGET_DIR
 
-# remove customizes options
-find . -name "*.proto" -exec sed -i '/option go_package =.*/d' {} \;
+# fix go_package option in proto files.
+find $PROTO_TARGET_DIR -name "*.proto" -exec sed -i 's|samaritan-proxy/samaritan/pb|samaritan-proxy/samaritan-api/go|g' {} \;
+# fix pakcage import path in go files.
+find $GO_TARGET_DIR -name "*.go" -exec sed -i 's|samaritan-proxy/samaritan/pb|samaritan-proxy/samaritan-api/go|g' {} \;
+# fix go module name
+sed -i 's|samaritan-proxy/samaritan|samaritan-proxy/samaritan-api/go|g' $GO_TARGET_DIR/go.mod
+pushd $GO_TARGET_DIR && go mod tidy && popd
 
 # push to git repo
+pushd $DIR
 git config user.name "sam-ci-bot"
 git config user.email sam-ci-bot@users.noreply.github.com
 diff=$(git --no-pager diff)
